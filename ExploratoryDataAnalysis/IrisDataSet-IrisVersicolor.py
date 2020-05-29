9e1cb28bfed1037d87e14acf1d5bc8fcdb0d8cf7
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-05-29 11:35:57 -0400

    First code commit

---
+++ ExploratoryDataAnalysis/IrisDataSet-IrisVersicolor.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+#importing the data from the folder
+iris_df = pd.read_csv('/Users/aakarsh.rajagopalan/Personal documents/Datasets for tableau/Iris length dataset/IRIS.csv')
+
+#printing head
+print(iris_df.head(5))
+print(iris_df['species'].unique())
+
+#creating a dataframe to store the iris versicolor subset.
+iris_df_versi = iris_df[iris_df['species'] == 'Iris-versicolor']
+print(iris_df_versi.head(5))
+
+#storing petal length of iris versicolor in a different variable
+iris_versi_petal_length = list(iris_df_versi['petal_length'])
+print(iris_versi_petal_length)
+
+#ploting the histogram on petal lengths of iris versicolor
+plt.hist(iris_versi_petal_length)
+plt.show()
